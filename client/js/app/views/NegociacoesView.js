4beca8bc467e7afa4fa12f71c22b675194df1304
jcperuggia <jessica.peruggia@hotmail.com>
2020-04-14 13:52:41 -0300

    somando o valor total das negociações

--- client/js/app/views/NegociacoesView.js
+++ client/js/app/views/NegociacoesView.js
@@ -21,21 +21,26 @@ class NegociacoesView {
 
             <tbody>
 
-                ${model.negociacoes.map(n => {
-                    return `
-                        <tr>
-                            <td>${DateHelper.dataParaTexto(n.data)}</td>
-                            <td>${n.quantidade}</td>
-                            <td>${n.valor}</td>
-                            <td>${n.volume}</td>
-                        </tr>
-                    `
-                }).jpoin('')}
+                ${model.negociacoes.map(n => `
+
+                    <tr>
+                        <td>${DateHelper.dataParaTexto(n.data)}</td>
+                        <td>${n.quantidade}</td>
+                        <td>${n.valor}</td>
+                        <td>${n.volume}</td>
+                    </tr>
+
+                `).jpoin('')}
 
             </tbody>
 
             <tfoot>
+                <td colspan="3"></td>
+                <td>
+                    ${model.negociacoes.reduce((total, n) => total + n.volume, 0.0)}
+                </td>
             </tfoot>
+            
         </table>
         `;
     }

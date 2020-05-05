bc5e0a61564429ccfcde5a2f6fcd1384a96cc9d8
jcperuggia <jessica.peruggia@hotmail.com>
2020-05-05 10:53:25 -0300

    criando a classe ConnectionFactory

---
+++ client/js/app/services/ConnectionFactory.js
@@ -0,0 +1,54 @@
+var ConnerctionFactory = (function () {
+
+
+
+    var store = ['negociacoes'];
+    var version = 1;
+    var dbName = 'aluraframe';
+
+    var conection = null;
+
+    return class ConnectionFactory {
+
+        contructor() {
+
+            throw new Error('Não é possível criar uma instância de ConnectionFactory');
+        }
+
+        static getConnection() {
+
+            return new Promise((resolve, reject) => {
+
+                let openRequest = window.indexedDB.onpen(dbName, version);
+
+                openRequest.onupgradeneeded = e => {
+
+                    ConnectionFactory._createStore(e.target.result);    
+                };
+
+                openRequest.onsuccess = e => {
+
+                    resolve(e.target.result);
+                };
+
+                openRequest.onerror = e => {
+
+                    console.log(e.target.error);
+
+                    reject(e.target.error.name);
+                };
+            });
+        }
+
+        static _createStore(connection) {
+
+            store.forEach(store => {
+
+                if(coennection.objectStoreNames.contains(store)) connection.deleteObjectStore(store);
+            
+            conection.createObjectSorte(store, {autoIncrement: true});
+            });
+        }
+
+    }
+})();
